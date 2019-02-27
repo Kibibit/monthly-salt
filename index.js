e4ebb2a864430d42d3b09afe1fdce91f663d5d5d
Neil Kalman <neilkalman@gmail.com>
2019-02-28 00:53:58 +0200

    error should still return a badge

--- index.js
+++ index.js
@@ -20,16 +20,23 @@ module.exports = async function (req, res) {
     res.setHeader('Content-Type', `application/json`);
     res.end(JSON.stringify({
       schemaVersion: 1,
-      label: 'bountysource',
-      message: `$${ monthlySum }`,
+      label: 'monthly salt',
+      message: `$ ${ monthlySum }`,
       color: "#33ccff",
       style: 'for-the-badge'
     }));
 
   } catch (e) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-    console.error('BIG BIG ERROR!!!!', e);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', `application/json`);
+    res.end(JSON.stringify({
+      schemaVersion: 1,
+      isError: true,
+      label: 'monthly salt',
+      message: `unknown`,
+      color: "#33ccff",
+      style: 'for-the-badge'
+    }));
+    console.error('Server Error!', e);
   }
 };
